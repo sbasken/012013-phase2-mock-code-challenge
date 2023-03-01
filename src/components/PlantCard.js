f809ea6d913d52d8c552019e4940573bc029229b
Saki <sbasken@gmail.com>
2023-03-01 10:03:50 -0800

    add initial render accessing API database, filter function, form to add new plants to the data base

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const { name, image, price } = plant
+  const [ isInStock, setInStock ] = useState(true)
+
+  const inStock = <button className="primary">In Stock</button>;
+  const soldOut = <button>Out of Stock</button>
+
+  const toggleButton = () => {
+    setInStock(isInStock => !isInStock)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      <label id="button" onClick={toggleButton}>{ isInStock ? inStock : soldOut}</label> 
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [ plants, setPlants ] = useState([])
+  const [ searchQuery, setSearchQuery ] = useState("")
+
+ function handleInputChange(e) {
+  setSearchQuery(e.target.value)
+ }
+
+ const filteredPlants = plants.filter(plant => (searchQuery === "") ? plants : plant.name.toLowerCase().includes(searchQuery.toLowerCase()))
+
+  const addPlant = (newPlant) => {
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants)
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(data => setPlants(data))
+  }, [])
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={addPlant}/>
+      <Search searchQuery={searchQuery} onInputChange={handleInputChange}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
